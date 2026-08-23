332748ee24aa36b6721b6c0cde18fd943b579fb3
Hafeez <hafeezmuhammad02807@gmail.com>
2026-08-23 15:33:12 +0530

    Comment out unused array handling code in learn.java

--- learn.java
+++ learn.java
@@ -85,22 +85,22 @@
 // }
 // }
 
-import java.util.*;
+// import java.util.*;
 
-class learn {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        System.out.println("Enter the elements in the array:  ");
-        ;
-        for (int i = 0; i <= n; i++) {
-            a[i] = sc.nextInt();
-        }
-        System.out.println("Elements of array are: ");
-        for (int i = 0; i <= 0; i++) {
-            System.out.println(a[i]);
-        }
-    }
-}
+// class learn {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the size of the array: ");
+//         int n = sc.nextInt();
+//         int a[] = new int[n];
+//         System.out.println("Enter the elements in the array:  ");
+//         ;
+//         for (int i = 0; i <= n; i++) {
+//             a[i] = sc.nextInt();
+//         }
+//         System.out.println("Elements of array are: ");
+//         for (int i = 0; i <= 0; i++) {
+//             System.out.println(a[i]);
+//         }
+//     }
+// }
