1ef11b8bfbb855c09d3538d3a8435f3ef3d28aa2
Hafeez <hafeezmuhammad02807@gmail.com>
2026-08-23 15:52:57 +0530

    Update learn.java

--- learn.java
+++ learn.java
@@ -1,54 +1,54 @@
 // public class learn {
-//     public static void main(String[] args) {
-//         int sum = 0;
-//         for (int i = 0; i <= 100; i++) {
-//             sum = sum + i;
-//         }
-//         System.out.println(sum);
-//     }
+// public static void main(String[] args) {
+// int sum = 0;
+// for (int i = 0; i <= 100; i++) {
+// sum = sum + i;
+// }
+// System.out.println(sum);
+// }
 // }
 
 // class learn {
-//     public static void main(String[] args) {
-//         for (int i = 0; i <= 10; i++) {
-//             if (i % 2 == 0) {
-//                 System.out.println(i);
-//             }
-//         }
-//     }
+// public static void main(String[] args) {
+// for (int i = 0; i <= 10; i++) {
+// if (i % 2 == 0) {
+// System.out.println(i);
+// }
+// }
+// }
 // }
 
 // class learn {
-//     public static void main(String[] args) {
-//         for (int i = 0; i <= 100; i++) {
-//             if (i % 2 == 1) {
-//                 System.out.println(i);
-//             }
-//         }
-//     }
+// public static void main(String[] args) {
+// for (int i = 0; i <= 100; i++) {
+// if (i % 2 == 1) {
+// System.out.println(i);
+// }
+// }
+// }
 // }
 
 // class learn {
-//     public static void main(String[] args) {
-//         int sum = 0;
-//         for (int i = 0; i <= 100; i++) {
-//             sum = sum + i;
-//         }
-//         System.out.println(sum);
-//     }
+// public static void main(String[] args) {
+// int sum = 0;
+// for (int i = 0; i <= 100; i++) {
+// sum = sum + i;
+// }
+// System.out.println(sum);
+// }
 // }
 
 // import java.util.*;
 
 // class learn {
-//     public static void main(String[] args) {
-//         System.out.println("Enter a number: ");
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         for (int i = 1; i <= 10; i++) {
-//             System.out.println(n + " * " + i + " = " + (n * i));
-//         }
-//     }
+// public static void main(String[] args) {
+// System.out.println("Enter a number: ");
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for (int i = 1; i <= 10; i++) {
+// System.out.println(n + " * " + i + " = " + (n * i));
+// }
+// }
 // }
 
 // class learn {
@@ -88,19 +88,51 @@
 // import java.util.*;
 
 // class learn {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter the size of the array: ");
-//         int n = sc.nextInt();
-//         int a[] = new int[n];
-//         System.out.println("Enter the elements in the array:  ");
-//         ;
-//         for (int i = 0; i <= n; i++) {
-//             a[i] = sc.nextInt();
-//         }
-//         System.out.println("Elements of array are: ");
-//         for (int i = 0; i <= 0; i++) {
-//             System.out.println(a[i]);
-//         }
-//     }
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the size of the array: ");
+// int n = sc.nextInt();
+// int a[] = new int[n];
+// System.out.println("Enter the elements in the array: ");
+// ;
+// for (int i = 0; i <= n; i++) {
+// a[i] = sc.nextInt();
+// }
+// System.out.println("Elements of array are: ");
+// for (int i = 0; i <= 0; i++) {
+// System.out.println(a[i]);
+// }
+// }
+// }
+
+// class Arrays {
+// public static void main(String[] args) {
+// int a[] = new int[4];
+// a[0] = 11;
+// a[1] = 22;
+// a[2] = 33;
+// a[3] = 44;
+// for (int i = 0; i < 4; i++) {
+// System.out.println(a[i]);
+// }
+// }
+// }
+
+// import java.util.*;
+// class Arrays {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the size of the array: ");
+// int n = sc.nextInt();
+// int a[] = new int[n];
+// System.out.println("Enter the elements in the array: ");
+// ;
+// for (int i = 0; i < n; i++) {
+// a[i] = sc.nextInt();
+// }
+// System.out.println("Elements of array are: ");
+// for (int i = 0; i < n; i++) {
+// System.out.println(a[i]);
+// }
+// }
 // }
